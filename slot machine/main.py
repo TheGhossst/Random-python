7ac9eb7e4d0bac98ae2465408271e7751f2fb00d
TheGhossst <nandukrishnapkm97@gmail.com>
2024-02-28 21:12:28 +0530

    add deposit

--- slot machine/main.py
+++ slot machine/main.py
@@ -11,3 +11,15 @@ def checkBalance():
         print("Devs credited $20 in your account")
         checkBalance()
     
+
+def deposit():
+    global amount
+    while True:
+        deposit_amount = int(input("Enter the amount: "))
+        if deposit_amount > 0:
+            amount += deposit_amount
+            print("Deposited ", deposit_amount)
+            checkBalance()
+            break
+        else:
+            print("Enter a number greater than 0")
