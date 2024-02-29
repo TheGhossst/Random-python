9abbe057b0b3562d9149a59e870fa7f0936660a8
TheGhossst <nandukrishnapkm97@gmail.com>
2024-02-29 19:18:43 +0530

    idk

--- Tic-Tac-Toe/game.py
+++ Tic-Tac-Toe/game.py
@@ -4,8 +4,16 @@ import random as rand
 def nextGame():
     print("New game")
     
+def checkWinner():
+    print("bah")
+    
 def nextTurn(row,column):
-    print(f"{i}{j}")
+    #print(f"{row}{column}")
+    global turn
+    
+    if board[row][column]['text'] == "":
+        if turn == players[0]:
+            board[row][column]['text'] = turn
     
 players = ["X","O"]
 
@@ -25,12 +33,17 @@ gameLabel.pack(side = "top" )
 nextGameButton = Button(window,text= 'Next Game', font=('Helvetica',16), command= nextGame)
 nextGameButton.pack(side = "top")
 
+turnLabel = Label(window,text = f"{turn}'s turn", font=('Arial', 20))
+turnLabel.pack(side = "top" )
+
+
 cellFrame = Frame(window)
 cellFrame.pack()
 
 for i in range(3):
     for j in range(3):
-        board[i][j] = Button(cellFrame,text = "",font = ('Helvetica',16),width = 5,height = 2 ,command = nextTurn  )
+        board[i][j] = Button(cellFrame, text="", font=('Helvetica', 16), width=5, height=2)
+        board[i][j].config(command=lambda row=i, column=j: nextTurn(row, column))
         board[i][j].grid(row = i, column = j)
 
 window.mainloop()
